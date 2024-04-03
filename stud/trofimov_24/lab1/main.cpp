67986f546f2773ef5a0fb7060adbf823398a2835
vampsh00ta <trofimov.vlad-1234@yandex.ru>
2024-04-03 13:00:35 +0300

    lab1.1 done

--- stud/trofimov_24/lab1/main.cpp
+++ stud/trofimov_24/lab1/main.cpp
@@ -1,6 +1,48 @@
+#include "matrix.hpp"
+#include <vector>
+#include <vector>
+#include <cstdio>
+
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    matrix A{
+            {-7,-2,-1,-4},
+            {-4,6,0,-4},
+            {-8,-2,-9,-3},
+            {0,0,-7,1},
+
+    };
+    matrix b {
+            {-12},
+            {22},
+            {51},
+            {49}
+    };
+    auto [L, U] = LU(A);
+
+    std::cout << std::endl << "L =" << endl;
+    print_matrix(L);
+
+    std::cout << std::endl << std::endl;
+    std::cout << "U =" << std::endl;
+    print_matrix(U);
+
+    std::cout << std::endl << std::endl;
+    std::cout << "Determinant = " << getDeterminant(A) << std::endl;
+
+    std::cout << std::endl << std::endl << "Roots =" << std::endl;
+
+    matrix res = solver(A,b);
+    print_matrix(res);
 
 
+    matrix AT = getInverse(A);
+    std::cout << std::endl << std::endl << "Reversed matrix =" << endl;
 
-void main(){
+    print_matrix(AT);
 
+    return 0;
 }
